bb4002840f06c69754feed027012a697d26629bd
Coldani3 <p110117069@gmail.com>
2021-12-13 14:43:11 +0000

    FINALLY fix ESLint for real.

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -6,6 +6,20 @@ const Staff = mongoose.model('Staff', StaffSchema);
 const Admins = mongoose.model('Admins', AdminSchema);
 const ClearingForms = mongoose.model('ClearingForms', ClearingFormSchema);
 
+const TokenLength = 15;
+
+function generateToken() {
+	let token = "";
+
+	for (let i = 0; i < TokenLength; i++) 
+	{
+		//based on https://stackoverflow.com/questions/8532406/create-a-random-token-in-javascript-based-on-user-details
+		token += Math.random().toString(36).substr(2);
+	}
+
+	return token;
+}
+
 export const login = (req, res) => {
 	const username = req.params.username;
 	const encryptedPassword = req.params.password;
